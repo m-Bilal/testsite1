b33f46394b056de7724159e104de7fc5b3357ddc
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-01-10 23:13:50 +0530

    Added models to process

--- process/models.py
+++ process/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    message = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return '%s %s'%(self.first_name, self.last_name)
