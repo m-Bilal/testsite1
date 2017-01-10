22f4b8c82a0af4c523e694ae644d2813c93e3666
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-01-10 23:45:16 +0530

    Added urls for web app process

--- testsite1/urls.py
+++ testsite1/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from testsite1 import views
+from process import views as process_veiws
+import process
+
+process_urlpatterns = [
+    url(r'^hello/$', process_veiws.hello)
+]
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', views.hello),
+    url(r'^hello/$', views.hello),
+    url(r'^process/', include(process_urlpatterns)),
 ]
